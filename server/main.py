127c1ab56b012f21e3137045b652ac2fd8c0a242
Antonin <81852696+Limit-sest@users.noreply.github.com>
2025-10-07 19:39:19 +0200

    Fix wrong items being selected

--- server/main.py
+++ server/main.py
@@ -8,11 +8,10 @@ console = Console()
 with console.screen():
   while True:
     print("Discovered ingredients:")
-    for ing in discovered:
-      console.print(f"[bright_black]{ing['id']}[/bright_black] {ing['emoji']} [b]{ing['name']}[/b]")
+    for i in range(len(discovered)):
+      ing = discovered[i]
+      console.print(f"[bright_black]{i+1}[/bright_black] {ing['emoji']} [b]{ing['name']}[/b]")
       console.print(f"   {ing["description"]}", style='italic bright_black')
-      # print(f"{ing["id"]} {ing["emoji"]} {ing["name"]}")
-      # print(f"   {ing["description"]}")
     print("\nWhat do you want to brew today? Enter numbers separated by space.")
     ingredients_raw = input('> ').split(' ')
     if len(ingredients_raw) < 2:
@@ -21,26 +20,27 @@ with console.screen():
     ingredients = []
     for ing in ingredients_raw:
       try:
-        ingredients.append(int(ing))
+        ingredients.append(discovered[int(ing)-1]['id'])
       except:
         continue
     
     existing = get_existing(ingredients)
     if existing:
       discovered.append(existing)
-      console.clear()
-      print("You have discovered a new item:")
-      console.print(f"[bright_black]{existing['id']}[/bright_black] {existing['emoji']} [b]{existing['name']}[/b]")
+      # console.clear()
+      print("You have discovered a new item!")
+      console.print(f"{existing['emoji']} [b]{existing['name']}[/b]")
       console.print(f"   {existing["description"]}\n", style='italic bright_black')
     else:
       print("Brewing...")
+      print(ingredients)
       new = create_new(ingredients)
       if (new == None):
         print("You didn't manage to combine these.")
       else:
         discovered.append(new)
-        console.clear()
-        print("You have discovered a new item:")
-        console.print(f"[bright_black]{new['id']}[/bright_black] {new['emoji']} [b]{new['name']}[/b]")
+        # console.clear()
+        print("You have discovered a new item!")
+        console.print(f"{new['emoji']} [b]{new['name']}[/b]")
         console.print(f"   {new["description"]}\n", style='italic bright_black')
         
