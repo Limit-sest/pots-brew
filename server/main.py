a9ac8bea26664b5b67433c2a3bfde859444cf42f
Antonin <81852696+Limit-sest@users.noreply.github.com>
2025-10-07 18:15:14 +0200

    Make cli usage better

--- server/main.py
+++ server/main.py
@@ -1,30 +1,46 @@
 from server import get_existing, create_new, get_initial
+from rich.console import Console
+from rich.text import Text
 
 discovered = get_initial()
+console = Console()
 
-while True:
-  print("Discovered ingredients:")
-  for ing in discovered:
-    print(f"{ing["id"]} {ing["emoji"]} {ing["name"]}")
-    print(f"   {ing["description"]}")
-  print("What do you want to brew today? Enter numbers separated by space.")
-  ingredients_raw = input('> ').split(' ')
-  if len(ingredients_raw) < 2:
-    print("Nothing happened.")
-    continue
-  ingredients = []
-  for ing in ingredients_raw:
-    try:
-      ingredients.append(int(ing))
-    except:
+with console.screen():
+  while True:
+    print("Discovered ingredients:")
+    for ing in discovered:
+      console.print(f"[bright_black]{ing['id']}[/bright_black] {ing['emoji']} [b]{ing['name']}[/b]")
+      console.print(f"   {ing["description"]}", style='italic bright_black')
+      # print(f"{ing["id"]} {ing["emoji"]} {ing["name"]}")
+      # print(f"   {ing["description"]}")
+    print("\nWhat do you want to brew today? Enter numbers separated by space.")
+    ingredients_raw = input('> ').split(' ')
+    if len(ingredients_raw) < 2:
+      print("Nothing happened.")
       continue
-  
-  existing = get_existing(ingredients)
-  if existing:
-    discovered.append(existing)
-  else:
-    new = create_new(ingredients)
-    if (new == None):
-      print("You didn't manage to combine these.")
+    ingredients = []
+    for ing in ingredients_raw:
+      try:
+        ingredients.append(int(ing))
+      except:
+        continue
+    
+    existing = get_existing(ingredients)
+    if existing:
+      discovered.append(existing)
+      console.clear()
+      print("You have discovered a new item:")
+      console.print(f"[bright_black]{existing['id']}[/bright_black] {existing['emoji']} [b]{existing['name']}[/b]")
+      console.print(f"   {existing["description"]}\n", style='italic bright_black')
     else:
-      discovered.append(new)
+      print("Brewing...")
+      new = create_new(ingredients)
+      if (new == None):
+        print("You didn't manage to combine these.")
+      else:
+        discovered.append(new)
+        console.clear()
+        print("You have discovered a new item:")
+        console.print(f"[bright_black]{new['id']}[/bright_black] {new['emoji']} [b]{new['name']}[/b]")
+        console.print(f"   {new["description"]}\n", style='italic bright_black')
+        
